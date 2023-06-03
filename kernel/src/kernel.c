426c9ad225200292d33a23e92c886a22d527a1b8
lewislee0920 <62365146+lewislee0920@users.noreply.github.com>
2023-06-03 08:46:19 -0300

    Update kernel.c

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -9,67 +9,8 @@ int main(int argc, char *argv[])
 	char *path = strdup(argv[1]);
 
 	iniciar_config(path);
-	conexion();
 
     return EXIT_SUCCESS;
-}
-
-void conexion(void)
-{
-    int socket_kernel_serv = iniciar_servidor(config_kernel.ip_kernel,config_kernel.puerto_escucha);
-    log_warning(un_logger, "Inicializando modulo KERNEL ... ");
-
-    int socket_proceso = 0;
-
-    conectar_con_cpu(socket_kernel_serv);
-    conectar_con_memoria(socket_kernel_serv);
-    conectar_con_filesystem(socket_kernel_serv);
-
-    log_warning(un_logger, "KERNEL a la espera de procesos ... ");
-}
-
-int conectar_con_cpu(int socket_kernel_serv)
-{
-    socket_cpu = crear_conexion(config_kernel.ip_cpu, config_kernel.puerto_cpu);
-    log_info(logger, "Enviando HANDSHAKE a CPU \n");
-    enviar_handshake(&socket_cpu, KERNEL);
-
-    return esperar_handshake(&socket_cpu, confirmar_modulo);
-}
-
-int conectar_con_memoria(int socket_kernel_serv)
-{
-    socket_memoria = crear_conexion(config_kernel.ip_memoria,config_kernel.puerto_memoria);
-    log_info(logger,"Enviando HANDSHAKE a MEMORIA");
-    enviar_handshake(&socket_memoria,KERNEL);
-
-    return esperar_handshake(&socket_memoria,confirmar_modulo);
-}
-
-int conectar_con_filesystem(int socket_kernel_serv)
-{
-    socket_filesystem = crear_conexion(config_kernel.ip_filesystem, config_kernel.puerto_cpu);
-    log_info(logger, "Enviando HANDSHAKE a CPU \n");
-    enviar_handshake(&socket_filesystem, KERNEL);
-
-    return esperar_handshake(&socket_filesystem, confirmar_modulo);
-}
-
-void confirmar_modulo(int *socket, modulo un_modulo) {
-    if(un_modulo == CPU) {
-       log_info(logger, "Handshake exitoso");
-    }
-    else if(un_modulo == MEMORIA) {
-       log_info(logger,"Handshake exitoso");
-    }
-    else if(un_modulo == FILESYSTEM) {
-       log_info(logger,"Handshake exitoso");
-    }
-    else {
-       log_error(logger,"Fallo Handshake",
-       obtener_nombre_modulo(un_modulo));
-       exit(EXIT_FAILURE);
-    }
 }
 
 void iniciar_config(char *path_config) {
